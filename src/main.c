8f55a61ffda8ac31e9c8b3a39aaea83ecc94587e
yikedz <huguangwei999@163.com>
2013-06-02 20:48:51 +0800

    更正脉冲输出

--- src/main.c
+++ src/main.c
@@ -1,74 +1,52 @@
-/*
-整理描述 ：
-    当输入端输入一个下降沿 低电平保持时间为2秒，
-    就会在对应输出端出现一个2秒的高电平，然后该输出端转到低电平。
-    若输入端低电平保持时间超过30MIN，
-    对应端再输出一次2秒的高电平，周而复始。
-*/
-
 #include"STC11.h"
 #include "time.h"
-
-#define HIGH_CON 1
-#define LOW_CON 0
+//-----------------------
 #define TIME_OUTPUT 12000
-#define OUTPUT_FOLLOW 1
-
-//#define HIGH 1
 #define LOW 1
-
-
 #define OUT_2MIN_DEF  output[6]
-//#define min2_output output[6]
+//-----------------------
+sbit low_high_input_select = P1^5;
+//-----------------------
 //输入信号
 sbit IN1 = P3^2;
 sbit IN2 = P3^3;
 sbit IN3 = P3^4;
-sbit IN4 = P3^5;  
+sbit IN4 = P3^5; 
 sbit IN5 = P3^7;
-
 sbit IN6 = P3^0;
-sbit OUT6 = P3^1;
-
-sbit low_high_input_select = P1^5;
-
-//输出信号 控制指示灯
+//-----------------------
 sbit OUT1 = P1^0;
 sbit OUT2 = P1^1;
 sbit OUT3 = P1^2;
 sbit OUT4 = P1^3;
 sbit OUT5 = P1^4;
-
+sbit OUT6 = P3^1;
+//----------------------
 sbit OUT_2MIN = P1^6;//2分钟后输出
-
 sbit CLEAR_ALARM = P1^7;//报警解除输入
-
-//unsigned char input[7] = {1,1,1,1,1,1,1};//输入缓冲
+//-----------------------
 unsigned char input[7] = {0,0,0,0,0,0,0};//输入缓冲
 unsigned char output[7] = {0,0,0,0,0,0,0};//输出缓冲
-
+unsigned char input_valid[5] = {0,0,0,0,0};
+unsigned long count_10ms[6]={0,0,0,0,0,0};
+unsigned char flg_2min_come[6]={0,0,0,0,0,0};
+//-----------------------
 unsigned char min2_output = 0;
 unsigned char flg_10ms = 0;
-
 unsigned char flg_10ms2 = 0;
 unsigned char output_state = 1;
 unsigned char output_level = 1;
-static unsigned int output_state_high_count = 0;
-static unsigned int output_state_low_count = 0;
+unsigned int output_state_high_count = 0;
+unsigned int output_state_low_count = 0;
 unsigned char output_pulse_count  = 0;
-
-unsigned long count_10ms[6]={0,0,0,0,0,0};
-unsigned char flg_2min_come[6]={0,0,0,0,0,0};
-
-
-void init_timer0(void);
+//-----------------------
 void init_port(void);
+void init_timer0(void);
 void system_handle(void);
 void io_handle(void);
 void output_2min_handle(void);
-
-unsigned char input_valid[5] = {0,0,0,0,0};
-
+void wait_get_input(void);
+//-----------------------
 void main(void)
 {
    init_port();//初始化io口
@@ -77,35 +55,19 @@ void main(void)
    
    EA =1;
 
+   wait_get_input();//等待获取输入信号
+
    while(1)
     {
-   	 system_handle();
-
-	 if(flg_10ms2)
-	 {
-	 	 flg_10ms2 = 0;
-
-		if(min2_output)
-		{
-			output_2min_handle();
-		}
-		else
-		{
-			   OUT_2MIN_DEF = 0;
-		}
-
-	 }
-
-
+		 system_handle();
+		 output_2min_handle();
     }
 }
-
 
 void init_port(void)
 {
 	P1M0 = 0x40;
 	P1M1 = 0;
-
 	OUT1 = 0;
 	OUT2 = 0;
 	OUT3 = 0;
@@ -113,21 +75,17 @@ void init_port(void)
 	OUT5 = 0;
 	OUT6 = 0;
 	OUT_2MIN = 0;
-	//sign_timer(12000,delay_ouput,1);
-	//while(1);
-
 }
 
 void timer0_int() interrupt 1
 {
   //12M 10MS
   TH0 = 0xD8;
-
   TL0 = 0xF0;
   flg_10ms = 1;
   flg_10ms2 = 1;
   //schedule_timer();
- // time_count_30min();
+  //time_count_30min();
   io_handle();
  
  }
@@ -142,19 +100,20 @@ void init_timer0(void)
 
 //信号处理程序
 void system_handle(void)
-{
+{											
 	static unsigned char system_state = 1;
 
 	static unsigned char  sub_system_state[5]={1,1,1,1,1};
+	
 	unsigned char i =0;
-	switch(system_state)
+	
+	if(flg_10ms)
 	{
-		case 1:
-			//输入 输出 对应通道处理
-			if(flg_10ms)//10ms置位一次
-			{
-				flg_10ms = 0;//须在此清零
-		
+		flg_10ms = 0;//须在此清零
+		switch(system_state)
+		{
+			case 1:
+			//P2 = system_state;
 				//控制输出端输出
 				if((input[0]==LOW)||(input[1]==LOW)||(input[2]==LOW)||(input[3]==LOW)||(input[4]==LOW))
 				{	
@@ -174,21 +133,18 @@ void system_handle(void)
 						{
 							flg_2min_come[i] = 1;
 							count_10ms[i] =0;
-						}
-						
+						}	
 					}
 					else
 					{	
 						count_10ms[i] = 0;
-		
 						if(flg_2min_come[i])
 						{
 						   flg_2min_come[i] = 0;
 						}
 					}
 				}
-		
-		
+
 				if(flg_2min_come[0]==1||flg_2min_come[1]==1||flg_2min_come[2]==1||flg_2min_come[3]==1||flg_2min_come[4]==1)
 				{
 					min2_output = 1;
@@ -197,99 +153,92 @@ void system_handle(void)
 				{
 					min2_output = 0;	
 				}
-			
+			break;
+			case 2:
+			break;
+			case 3:
+			//P2 = system_state;
+			for(i=0;i<5;i++)
+			{
+				switch(sub_system_state[i])
+				{
+					case 1:
+						if(input[i]==LOW)//没有释放
+						{
+							input_valid[i] = 0;		
+						}
+						else
+						{
+							input_valid[i] = 0;	
+							sub_system_state[i] = 2;
+						}
+					break;
+					case 2:
+						if(input[i]==LOW)
+						{
+							input_valid[i] = 1;	
+						}
+						else
+						{
+							input_valid[i] = 0;
+						}
+						   //enable_output[i] = 1;
+					break;
+					default:
+						sub_system_state[i] = 1;
+					break;
+				}			
 			}
-		break;
-		case 2:
-		break;
-		case 3:
-		for(i=0;i<5;i++)
-		{
-			switch(sub_system_state[i])
-			{
-				case 1:
-					if(input[i]==LOW)//没有释放
-					{
-						input_valid[i] = 0;		
-					}
-					else
-					{
-						input_valid[i] = 0;	
-						sub_system_state[i] = 2;
-					}
-				break;
-				case 2:
-					if(input[i]==LOW)
-					{
-						input_valid[i] = 1;	
-					}
-					else
-					{
-						input_valid[i] = 0;
-					}
-					   //enable_output[i] = 1;
-				break;
-				default:
-					sub_system_state[i] = 1;
-				break;
-			}			
-		}
 
-
-			//输入 输出 对应通道处理
-			if(flg_10ms)//10ms置位一次
+			//控制输出端输出
+			if((input_valid[0]==1)||(input_valid[1]==1)||(input_valid[2]==1)||(input_valid[3]==1)||(input_valid[4]==1))
+			{	
+				output[5] = 1;
+			}
+			else
 			{
-				flg_10ms = 0;//须在此清零
-		
-				//控制输出端输出
-				if((input_valid[0]==1)||(input_valid[1]==1)||(input_valid[2]==1)||(input_valid[3]==1)||(input_valid[4]==1))
+				output[5] = 0;
+			}
+			//2min延时
+			for(i=0;i<5;i++)
+			{
+				if(input_valid[i]==1)
+				{
+					count_10ms[i]++;
+					if(count_10ms[i]==TIME_OUTPUT)
+					{
+						flg_2min_come[i] = 1;
+						count_10ms[i] =0;
+					}
+							
+				}
+				else
 				{	
-					output[5] = 1;
-				}
-				else
-				{
-					output[5] = 0;
-				}
-				//2min延时
-				for(i=0;i<5;i++)
-				{
-					if(input_valid[i]==1)
-					{
-						count_10ms[i]++;
-						if(count_10ms[i]==TIME_OUTPUT)
-						{
-							flg_2min_come[i] = 1;
-							count_10ms[i] =0;
-						}
-						
-					}
-					else
-					{	
-						count_10ms[i] = 0;
-		
-						if(flg_2min_come[i])
-						{
-						   flg_2min_come[i] = 0;
-						}
-					}
-				}
-		
-		
-				if(flg_2min_come[0]==1||flg_2min_come[1]==1||flg_2min_come[2]==1||flg_2min_come[3]==1||flg_2min_come[4]==1)
-				{
-					min2_output = 1;
-				}
-				else
-				{
-					min2_output = 0;	
-				}
+					count_10ms[i] = 0;
 			
+					if(flg_2min_come[i])
+					{
+					   flg_2min_come[i] = 0;
+					}
+				}
 			}
 
-		break;
+			if(flg_2min_come[0]==1||flg_2min_come[1]==1||flg_2min_come[2]==1||flg_2min_come[3]==1||flg_2min_come[4]==1)
+			{
+				min2_output = 1;
+			}
+			else
+			{
+				min2_output = 0;	
+			}
+			break;
+			
+			default:
+				system_state = 1; 
+			break;
+		}
+		
 	}
-
-
 	//消音按键按下
 	if(input[6]==0)
 	{
@@ -476,67 +425,92 @@ void io_handle(void)
 	 OUT_2MIN = (bit)output[6];
 }
 
-
 void output_2min_handle(void)
 {
-	switch(output_state)
-	{
-		case 1://前5个脉冲输出
-			if(output_level)//输出电平高低
-			{
-				OUT_2MIN_DEF = 1;
-				output_state_high_count++;
-				if(output_state_high_count==200)
-				{
-					output_state_high_count = 0;
-					output_level = 0;
-				}
-			}
-			else//(output_level==0)
-			{
-				OUT_2MIN_DEF = 0;
-				output_state_low_count++;
-				if(output_state_low_count==100)
-				{
-					output_state_low_count = 0;
-					output_level = 1;
-					
-					output_pulse_count++;
-					if(output_pulse_count==5)
-					{
-						output_state = 2;
-						output_pulse_count = 0;
-					}					
-				}
+		 if(flg_10ms2)
+		 {
+			 flg_10ms2 = 0;
 
-			}
-		break;
-		case 2:
-			if(output_level)//输出电平高
+			if(min2_output)
 			{
-				OUT_2MIN_DEF = 1;
-				output_state_high_count++;
-				if(output_state_high_count==500)
+				switch(output_state)
 				{
-					output_state_high_count = 0;
-					output_level = 0;
-				}
-			}
-			else//(output_level==0)
-			{
-				OUT_2MIN_DEF = 0;
-				output_state_low_count++;
-				if(output_state_low_count==200)
-				{
-					output_state_low_count = 0;
-					output_level = 1;
+					case 1://前5个脉冲输出
+						if(output_level)//输出电平高低
+						{
+							OUT_2MIN_DEF = 1;
+							output_state_high_count++;
+							if(output_state_high_count==200)
+							{
+								output_state_high_count = 0;
+								output_level = 0;
+							}
+						}
+						else//(output_level==0)
+						{
+							OUT_2MIN_DEF = 0;
+							output_state_low_count++;
+							if(output_state_low_count==100)
+							{
+								output_state_low_count = 0;
+								output_level = 1;
+								
+								output_pulse_count++;
+								if(output_pulse_count==5)
+								{
+									output_state = 2;
+									output_pulse_count = 0;
+								}					
+							}
+
+						}
+					break;
+					case 2:
+						if(output_level)//输出电平高
+						{
+							OUT_2MIN_DEF = 1;
+							output_state_high_count++;
+							if(output_state_high_count==500)
+							{
+								output_state_high_count = 0;
+								output_level = 0;
+							}
+						}
+						else//(output_level==0)
+						{
+							OUT_2MIN_DEF = 0;
+							output_state_low_count++;
+							if(output_state_low_count==200)
+							{
+								output_state_low_count = 0;
+								output_level = 1;
+								
+								output_state = 1;
+							}
+						}		
+					break;
 					
-					output_state = 1;
-				}
-			}		
-		break;
-		
-	}
-	
+				}			
+			}
+			else
+			{
+				   OUT_2MIN_DEF = 0;
+			}
+		 }
 }
+void wait_get_input(void)
+{
+	static unsigned char wait_time = 20;
+	
+	while(wait_time)
+	{
+		if(flg_10ms)
+		{
+			flg_10ms = 0;
+			wait_time--;
+		}
+	}
+
+}
+
 
